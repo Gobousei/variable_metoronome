3c7c71311d6b30082c80605153a265a58db8a7cb
Gobousei <93245017+Gobousei@users.noreply.github.com>
2024-08-17 18:36:30 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-
+//**用語解説：小節群・・・その関数で取り扱っている小節の集合で、速さや拍子などまとまりがあるもの */
+//**セル・・・メトロノームとして使用している画面上部の並んだ四角形 */
 const buttonStart = document.querySelector('#buttonStart')
 const buttonStop = document.querySelector('#buttonStop')
 const buttonReset = document.querySelector('#buttonReset')
@@ -14,11 +15,8 @@ var videodata;
 table.width='100%';
 buttonReset.style.display='none'
 buttonStop.style.display='none'
-for(l=0;l<3;l++){
-  table.cells[l].width= '333px';
-  }
 
-  var ua = navigator.userAgent;
+  var ua = navigator.userAgent;//スマートフォンでの使用禁止
 	if (ua.indexOf('iPhone') > 0
 			|| (ua.indexOf('Android') > 0) && (ua.indexOf('Mobile') > 0)
 			|| ua.indexOf('Windows Phone') > 0) {
@@ -82,23 +80,23 @@ main()
 
 function setting(){//設定の読み込みとカウントの開始
   array_location++;
-    if(array_location == -1){
+    if(array_location == -1){//予備小節
       let bpm = param[0][0];
       let beats = param[0][1];
       memory = '演奏開始';
-      count(bpm,'予備3小節',beats,3,'次の小節から演奏',false);
-    }else{
+      count(bpm,'予備3小節',beats,3,'演奏',false);
+    }else{//予備小節以降の設定
       let bpm = param[array_location][0];
       let display = memory;
       let beats = param[array_location][1];
       let measure = param[array_location][2];
       let fin;
-      if(param.length == array_location+1){
+      if(param.length == array_location+1){//次の配列がない場合、この小節群で終了する
         fin = true;
       }else{
         fin = false;
       }
-      if(!fin){
+      if(!fin){//この小節群が最後でないなら、次の小節群での変更点を記録する
         let next_location = array_location+1;
         switch(true){
           case param[array_location][0] != param[next_location][0]:
@@ -122,47 +120,47 @@ function count(bpm,display,beats,measure,next,fin){//カウント
 
   setTimeout(function(){
     text.innerText = display;
-    let balance_cell = beats - table.cells.length; //現在のセル数を取得し、必要なセル数に調整する
+    let balance_cell = beats - table.cells.length; //現在のセル数を取得し、セルの過不足を計算する
+    console.log(balance_cell);
   if(balance_cell > 0){
-    table.insertCell(balance_cell);
+    for(let c = 0; c<balance_cell;c++){
+    table.insertCell(0);
+    }
   }else if(balance_cell < 0){
     table.deleteCell(balance_cell);
-  }
-  table.width='100%';
-  for(l=0;l<beats-1;l++){
-    table.cells[l].width= 1000/beats+'px';
   }
   },ms);
   
   
   let decell = beats-1;
-  const timer = setInterval(function() {
+  const timer = setInterval(function() {//カウントの本体、この部分を小節数分繰り返す
     i++;
-    table.cells[i].style.backgroundColor = "yellow";
-    if(i!=0){
+    table.cells[i].style.backgroundColor = "yellow";//左のセルから順番に黄色にする
+    if(i!=0){//一つ左のセルの色を白に戻す
       let h = i-1;
       table.cells[h].style.backgroundColor = "white";
     }
-    switch (true){
-      case i==0:
+    switch (true){//特定の拍数の時のみに発動する処理
+      case i==0://小節頭では一番右のセルを白色にする
         table.cells[decell].style.backgroundColor = "white";
       break;
 
-      case i==decell:
+      case i==decell://小節最後では拍数カウントをリセットし、小節数カウントを一つ減らす
         i=-1;
         measure--;
       break;
     }
     
-    if(measure==1){
+    if(measure==1&&!fin){//次の小節群での変更点を表示する
       setTimeout(function(){text.innerText = '次の小節から'+next},ms);
     }
-    if (measure <= 0) {
+    if (measure <= 0) {//小節数カウントが0になったら
       clearInterval(timer);
-      if(fin){
+      if(fin){//この小節群が最後の場合、演奏終了を表示する
       setTimeout(function(){text.innerText = '演奏終了'},ms);
-      }
+      }else{//最後でないなら、次のカウントの読み込みを行う
       setting();
+      }
     }
   }, ms);
 }
